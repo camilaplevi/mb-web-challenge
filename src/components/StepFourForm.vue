d49ec1bd8d0971d7d65ac2ddd2467d9cc30c0662
Camila Pires Levi <camilaplevi@gmail.com>
2025-01-19 15:25:30 -0300

    feat/Adição do input radio para visualização da senha

--- src/components/StepFourForm.vue
+++ src/components/StepFourForm.vue
@@ -1,9 +1,14 @@
 <script setup>
 import { ref, defineProps } from 'vue';
 
-const currentStep = ref(4);
+const currentStep = ref(4)
+const showPassword = ref(false);
 
-const emit = defineEmits(['back', 'submit']);
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
+};
+
+const emit = defineEmits(['back', 'submit'])
 
 const { userType, formData } = defineProps({
   userType: {
@@ -14,18 +19,16 @@ const { userType, formData } = defineProps({
     type: Object,
     required: true,
   },
-});
-console.log(formData)
-
+})
 
 const back = () => {
-    emit('back');
+  emit('back')
 };
 
 const submit = () => {
-  alert('Cadastro finalizado com sucesso!');
-  emit('next');
-};
+  alert('Cadastro finalizado com sucesso!')
+  emit('next')
+}
 
 </script>
 
@@ -45,10 +48,14 @@ const submit = () => {
         <input v-model="formData.birthdate" id="birthdate" type="date" required />
         <label for="phone">Telefone:</label>
         <input v-model="formData.phone" id="phone" type="text" required />
-        <label for="phone">Senha:</label>
-        <input v-model="formData.password" id="phone" type="password" required />
+        <label for="password">Senha:</label>
+        <input v-model="formData.password" id="password" :type="showPassword ? 'text' : 'password'" required />
+        <div id="inputPassword">
+          <input type="radio" :checked="showPassword" @click="togglePasswordVisibility" class="toggle-button">
+          <label for="password" id="labelPassword">{{ showPassword ? 'Ocultar Senha' : 'Mostrar Senha' }}</label>
+        </div>
       </div>
-      
+
       <div v-if="userType === 'pj'">
         <label for="name">Endereço de e-mail:</label>
         <input v-model="formData.email" id="name" type="text" required />
@@ -60,10 +67,13 @@ const submit = () => {
         <input v-model="formData.openingDate" id="openingDate" type="date" required />
         <label for="phone">Telefone:</label>
         <input v-model="formData.phone" id="phone" type="text" required />
-        <label for="phone">Senha:</label>
-        <input v-model="formData.password" id="phone" type="password" required />
+        <label for="password">Senha:</label>
+        <input v-model="formData.password" id="password" :type="showPassword ? 'text' : 'password'" required />
+        <div id="inputPassword">
+          <input type="radio" :checked="showPassword" @click="togglePasswordVisibility" class="toggle-button">
+          <label for="password" id="labelPassword">{{ showPassword ? 'Ocultar Senha' : 'Mostrar Senha' }}</label>
+        </div>
       </div>
-
       <div class="buttons">
         <button type="button" id="back" @click="back">Voltar</button>
         <button type="submit">Cadastrar</button>
@@ -89,5 +99,43 @@ const submit = () => {
   .currentStep 
     color:  #ff942f
     font-weight: bold
+
+  #inputPassword
+    display: flex
+    align-items: center
+    justify-content: flex-start
+    gap: 8px
+    padding-top: 5px
+
+    .toggle-button
+      appearance: none
+      width: 20px
+      height: 20px
+      border: 1px solid #213547
+      border-radius: 50%
+      outline: none
+      cursor: pointer
+      display: inline-block
+      position: relative
+      background-color: transparent
+      padding-top: 10px
+
+      &::after
+        content: ''
+        position: absolute 
+        width: 10px
+        height: 10px
+        border-radius: 50%
+        background-color: black
+        top: 50%
+        left: 50%
+        transform: translate(-50%, -50%) 
+
+        &:not(:checked)
+          background-color: transparent
+
+  #labelPassword
+    cursor: pointer
+    font-size: 14px
   
   </style>
